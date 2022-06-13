e18bd3fc3e4a13839b0ed1eeff29da080c417b95
MakrFrost <frostmark@ukr.net>
2022-06-13 17:53:58 +0300

    Add js files

---
+++ js/burger.js
@@ -0,0 +1,10 @@
+const burgerMenu = (option) => {
+  const {
+    selectorBurger = ".burger",
+    activeBurger = ".burger_active",
+    selectorMenu,
+    openMenuSelector,
+  } = option;
+};
+
+export default burgerMenu;
---
+++ js/script.js
@@ -0,0 +1,6 @@
+import burgerMenu from "./burger.js";
+
+burgerMenu({
+  selectorMenu: ".header-nav__list",
+  openMenuSelector: ".header-nav__list_active",
+});
