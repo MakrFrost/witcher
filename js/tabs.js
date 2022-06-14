485946ffca12546ffba6b9ba1c1f65d41dbb4656
MakrFrost <frostmark@ukr.net>
2022-06-14 18:11:41 +0300

    Update JS menu

--- js/tabs.js
+++ js/tabs.js
@@ -7,11 +7,21 @@ const tabs = (option) => {
     callback = () => {},
   } = option;
 
-  const tabsBtn = document.querySelectorAll(selectorTabsButtons);
-  const tabsElem = document.querySelectorAll(selectorTabsElement);
+  const tabsBtns = document.querySelectorAll(selectorTabsButtons);
+  const tabsElems = document.querySelectorAll(selectorTabsElement);
 
-  tabsBtns.forEach((btn, i) => {
-    console.log(btn, i);
+  tabsBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      for (let i = 0; i < tabsBtns.length; i++) {
+        if (tabsBtns[i] === btn) {
+          tabsBtns[i].classList.add(activeClassButton);
+          tabsElems[i].classList.add(activeClassTab);
+        } else {
+          tabsBtns[i].classList.remove(activeClassButton);
+          tabsElems[i].classList.remove(activeClassTab);
+        }
+      }
+    });
   });
 };
 
